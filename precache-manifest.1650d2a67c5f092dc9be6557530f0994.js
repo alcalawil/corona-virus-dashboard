a0f9303b0608745f3de44735b99481069b3c8613
Wilfredo Alcala <alcalawilfre@gmail.com>
2020-04-05 23:32:08 -0300

    Updates

---
+++ precache-manifest.1650d2a67c5f092dc9be6557530f0994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11a323ba16456e0b5a9a3cbd5a97738",
+    "url": "/corona-virus-dashboard/index.html"
+  },
+  {
+    "revision": "13e12d698ec8028f143d",
+    "url": "/corona-virus-dashboard/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "797d6314416f94365b64",
+    "url": "/corona-virus-dashboard/static/css/main.fb76e71a.chunk.css"
+  },
+  {
+    "revision": "13e12d698ec8028f143d",
+    "url": "/corona-virus-dashboard/static/js/2.5ea2d944.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/corona-virus-dashboard/static/js/2.5ea2d944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797d6314416f94365b64",
+    "url": "/corona-virus-dashboard/static/js/main.1c1ba81c.chunk.js"
+  },
+  {
+    "revision": "acd41b2de36f67ec74b9",
+    "url": "/corona-virus-dashboard/static/js/runtime-main.dda8d0c7.js"
+  }
+]);
