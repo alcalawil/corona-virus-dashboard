ecd00f91aecc74eab2d71c72dbca6e0f46c21148
Wilfredo Alcala <alcalawilfre@gmail.com>
2020-04-14 19:08:32 -0300

    ref(footer): update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,25 +9,22 @@ class Footer extends Component {
           <nav className="pull-left">
             <ul>
               <li>
-                <a href="#pablo">Home</a>
+                <a href="#">Inicio</a>
               </li>
               <li>
-                <a href="#pablo">Company</a>
-              </li>
-              <li>
-                <a href="#pablo">Portfolio</a>
-              </li>
-              <li>
-                <a href="#pablo">Blog</a>
+                <a href="https://github.com/alcalawil">Portfolio</a>
               </li>
             </ul>
           </nav>
           <p className="copyright pull-right">
             &copy; {new Date().getFullYear()}{" "}
-            <a href="http://www.creative-tim.com?ref=lbr-footer">
-              Creative Tim
+            <a
+              href="https://www.linkedin.com/in/wilfredo-alcala/"
+              target="_blank"
+            >
+              by Wilfredo Alcala
             </a>
-            , made with love for a better web
+            , en casa
           </p>
         </Grid>
       </footer>
